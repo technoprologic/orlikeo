7a256ee1956f392a70b2a7ec70f77cf6a17f1e22
emagdnim <mariusz.zych@o2.pl>
2014-11-19 20:15:07 +0100

    home controller to "/"

--- src/main/java/umk/zychu/inzynierka/controller/HelloController.java
+++ src/main/java/umk/zychu/inzynierka/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("/")
 public class HelloController {
 
 	@RequestMapping(method = RequestMethod.GET)
