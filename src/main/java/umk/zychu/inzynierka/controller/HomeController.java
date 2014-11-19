c3cae067dcc603289b9d35720428a0de89f7d66f
emagdnim <mariusz.zych@o2.pl>
2014-11-19 20:34:10 +0100

    bootstrap connecting stylesheets

--- src/main/java/umk/zychu/inzynierka/controller/HomeController.java
+++ src/main/java/umk/zychu/inzynierka/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
-public class HelloController {
+public class HomeController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
