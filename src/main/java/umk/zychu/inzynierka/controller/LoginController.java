0b205a5daaabc996423be8f464ce84f45af3d501
emagdnim <mariusz.zych@o2.pl>
2014-11-23 16:15:35 +0100

    changes in templates

--- src/main/java/umk/zychu/inzynierka/controller/LoginController.java
+++ src/main/java/umk/zychu/inzynierka/controller/LoginController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class RegisterController {
+public class LoginController {
 
-	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public String registerNewUser(ModelMap model) {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
 
 		model.addAttribute("message", "Spring 3 MVC Hello World");
-		return "register";
+		return "login";
 
 	}
 
