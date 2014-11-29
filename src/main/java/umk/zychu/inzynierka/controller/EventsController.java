8827431b9be2bd0acbc56b10528d666eafac556b
emagdnim <mariusz.zych@o2.pl>
2014-11-30 00:30:59 +0100

    sa

--- src/main/java/umk/zychu/inzynierka/controller/EventsController.java
+++ src/main/java/umk/zychu/inzynierka/controller/EventsController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequestMapping(value="/events")
 public class EventsController {
@@ -14,14 +19,15 @@ public class EventsController {
 	
 	@RequestMapping(value="dupa",  method = RequestMethod.GET)
 	@ResponseBody
-	public String dupa(ModelMap model) {
+	public String dupa(HttpServletResponse response, ModelMap model) {
 
-		/*model.addAttribute("message", "Spring 3 MVC Hello World");*/
-		return "createEvent";
+		response.setContentType("text/plain");
+	    response.setCharacterEncoding("UTF-8");
+	    return "createEvent";
 	}
 
 	
-	@RequestMapping( value="/help2", method = RequestMethod.GET)
+	@RequestMapping( value="help2", method = RequestMethod.GET)
 	public String create2(ModelMap model) {
 
 		/*model.addAttribute("message", "Spring 3 MVC Hello World");*/
