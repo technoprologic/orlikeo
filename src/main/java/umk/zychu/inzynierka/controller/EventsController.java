f10dd8684e9d0205aa0a18234e7be2b4870a53a8
emagdnim <mariusz.zych@o2.pl>
2014-11-29 21:49:19 +0100

    sa

--- src/main/java/umk/zychu/inzynierka/controller/EventsController.java
+++ src/main/java/umk/zychu/inzynierka/controller/EventsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/events")
+//@RequestMapping(value = "/events")
 public class EventsController {
 
 	
