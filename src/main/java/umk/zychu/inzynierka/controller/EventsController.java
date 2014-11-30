5a5b2fee3dadf38ab4456685930171a86fcd95f4
emagdnim <mariusz.zych@o2.pl>
2014-11-30 13:12:15 +0100

    sa

--- src/main/java/umk/zychu/inzynierka/controller/EventsController.java
+++ src/main/java/umk/zychu/inzynierka/controller/EventsController.java
@@ -9,29 +9,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
+@RequestMapping(value="/kontroler")
 public class EventsController {
 
 	
 
-	@RequestMapping(value="/dupa", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String dupa2(ModelMap model) {
 
 	    return "home";
 	}
 	
 	
-	@RequestMapping(value="/template", method = RequestMethod.GET)
+	@RequestMapping(value="/metoda", method = RequestMethod.GET)
 	public String dupa(ModelMap model) {
 
 	    return "home";
 	}
-	
-	
-	@RequestMapping( value="/log", method = RequestMethod.GET)
-	public String create2(ModelMap model) {
 
-		/*model.addAttribute("message", "Spring 3 MVC Hello World");*/
-		return "profile";
-	}
 
 }
