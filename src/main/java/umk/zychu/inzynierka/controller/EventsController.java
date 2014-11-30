259c951ade8ae25504918e4bc9264c7548af236b
emagdnim <mariusz.zych@o2.pl>
2014-11-30 01:24:35 +0100

    sa

--- src/main/java/umk/zychu/inzynierka/controller/EventsController.java
+++ src/main/java/umk/zychu/inzynierka/controller/EventsController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -20,6 +21,7 @@ public class EventsController {
 
 	
 	@RequestMapping( value="/create", method = RequestMethod.GET)
+	@ResponseBody
 	public String create2(ModelMap model) {
 
 		/*model.addAttribute("message", "Spring 3 MVC Hello World");*/
