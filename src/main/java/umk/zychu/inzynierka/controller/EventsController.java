ae2eb4125662751f132c623f11caa6f610b6340e
emagdnim <mariusz.zych@o2.pl>
2014-11-30 01:11:30 +0100

    sa

--- src/main/java/umk/zychu/inzynierka/controller/EventsController.java
+++ src/main/java/umk/zychu/inzynierka/controller/EventsController.java
@@ -19,7 +19,7 @@ public class EventsController {
 	}
 
 	
-	@RequestMapping( value="help2", method = RequestMethod.GET)
+	@RequestMapping( value="/events/help2", method = RequestMethod.GET)
 	public String create2(ModelMap model) {
 
 		/*model.addAttribute("message", "Spring 3 MVC Hello World");*/
