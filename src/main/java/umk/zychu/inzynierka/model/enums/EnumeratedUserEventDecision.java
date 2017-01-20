1869604180b2b6350e2a163774d5d7236a65a7c3
mzych <technoprologic@gmail.com>
2017-01-20 23:48:59 +0100

    Orlik animator added to User.Builder class

--- src/main/java/umk/zychu/inzynierka/model/enums/EnumeratedUserEventDecision.java
+++ src/main/java/umk/zychu/inzynierka/model/enums/EnumeratedUserEventDecision.java
@@ -6,7 +6,7 @@ public enum EnumeratedUserEventDecision {
     INVITED(1),
     ACCEPTED(2),
     REJECTED(3),
-    //indicates user not invited but can invite
+    //indicates user is not invited but can invite (
     NOT_INVITED(4);
 
     private final int value;
